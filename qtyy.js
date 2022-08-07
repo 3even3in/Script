62316b93bd9d016df548983d2cab7317302a4566
3even3in <91725342+3even3in@users.noreply.github.com>
2022-08-07 17:14:50 +0800

    Update qtyy.js

--- qtyy.js
+++ qtyy.js
@@ -2,9 +2,9 @@
 脚本:倾听英语 解锁VIP  By：Seven3in
 [rewrite_local]
 Quantumult X 
-^https:\/\/sapi.beingfine.cn\/v1\/UserLaunchAppReport* url script-response-body https://gitee.com/seven3in/Script/raw/master/Bigshot.js
+^https:\/\/sapi.beingfine.cn\/v1\/UserLaunchAppReport* url script-response-body https://raw.githubusercontent.com/3even3in/Script/main/qtyy.js
 Loon
-http-response ^https:\/\/sapi.beingfine.cn\/v1\/UserLaunchAppReport* requires-body=1,script-path=https://gitee.com/seven3in/Script/raw/master/Bigshot.js,tag=Bigshot
+http-response ^https:\/\/sapi.beingfine.cn\/v1\/UserLaunchAppReport* requires-body=1,script-path=https://raw.githubusercontent.com/3even3in/Script/main/qtyy.js,tag=倾听英语
 [MITM]
 hostname = sapi.beingfine.cn
 */
@@ -12,4 +12,5 @@ hostname = sapi.beingfine.cn
 
 body = $response.body.replace(/granted":\d+/g, 'granted":1');
 body = body.replace(/expire_date":\d+/g, 'expire_date":17368358400');
+body = body.replace(/nickname":".+"/g, 'nickname":"Seven3in"');
 $done({body});
